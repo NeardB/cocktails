2a50f0ca7e7eea5924c808d45253d64305edbf84
NerdB <bojanar@gmail.com>
2015-06-27 23:39:25 +0200

    Promene
    Promeene

--- project.clj
+++ project.clj
@@ -6,7 +6,13 @@
                  [compojure "1.3.1"]
                  [ring/ring-defaults "0.1.2"]
                  [hiccup "1.0.2"]
-                 [org.clojure/java.jdbc "0.2.3"]
+
+                 [clj-enlive-template "0.0.1"]
+                 ;;[noir "1.3.0"]
+                 ;;[clj-json "0.5.3"]
+                 [enlive "1.1.5"]
+
+                 [org.clojure/java.jdbc "0.3.6"]
                  [mysql/mysql-connector-java "5.1.6"]
                  ]
   :plugins [[lein-ring "0.8.13"]]
